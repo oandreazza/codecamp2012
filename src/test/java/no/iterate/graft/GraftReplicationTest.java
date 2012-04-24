369bfddad85ecb26322d64ee0d7db7bc5c129b5e
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2012-04-24 09:19:03 -0700

    first test for replication

---
+++ src/test/java/no/iterate/graft/GraftReplicationTest.java
@@ -0,0 +1,32 @@
+package no.iterate.graft;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class GraftReplicationTest {
+
+	@Test
+	public void shouldReplicateGraft() {
+		// start up two grafts
+		List<Graft> grafts = Graft.getTwoGrafts();
+		Graft first = grafts.get(0);
+		Graft second = grafts.get(1);
+		
+		
+		// write some data to first graft
+		Node firstGraftNode = first.createNode();
+		firstGraftNode.put("key", "value");
+		// replicate data to second one in background
+		
+		// kill the first graft with shotgun
+		first.kill();
+		
+		// check replicated data in second graft
+		Node secondGraftnode = second.getNodeByProperty("key", "value");
+		assertNotNull(secondGraftnode);
+	}
+}
