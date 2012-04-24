54bed1c9e07177744492341710953a593f6136eb
stigberg <stig.bergestad@iterate.no>
2012-04-24 09:40:32 -0700

    Added first version of replicatin between Grafts

--- src/main/java/no/iterate/graft/Graft.java
+++ src/main/java/no/iterate/graft/Graft.java
@@ -2,15 +2,30 @@ package no.iterate.graft;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Graft {
+	private final List<Graft> replicas = new ArrayList<Graft>();
 
 	private final Collection<Node> nodes = new ArrayList<Node>();
 
 	public Node createNode() {
 		Node node = new Node();
 		nodes.add(node);
+
+		addNodeToReplicas(node);
+
 		return node;
+	}
+
+	private void addNodeToReplicas(Node node) {
+		for (Graft replica : replicas) {
+			replica.addReplicatedNode(node);
+		}
+	}
+
+	private void addReplicatedNode(Node node) {
+		nodes.add(node);
 	}
 
 	public Node getNodeByProperty(String property, String value) {
@@ -34,4 +49,23 @@ public class Graft {
 		return node.getEdges();
 	}
 
+	public static List<Graft> getTwoGrafts() {
+		List<Graft> grafts = new ArrayList<Graft>();
+		Graft graft1 = new Graft();
+		Graft graft2 = new Graft();
+		graft1.addReplica(graft2);
+		graft2.addReplica(graft1);
+		grafts.add(graft1);
+		grafts.add(graft2);
+
+		return grafts;
+	}
+
+	private void addReplica(Graft graft) {
+		replicas.add(graft);
+	}
+
+	public void kill() {
+		nodes.clear();
+	}
 }
