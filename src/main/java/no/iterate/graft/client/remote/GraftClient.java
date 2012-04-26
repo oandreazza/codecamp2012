35a6b1cb919eb322ad7e3ad6e81f86c2c2f4374c
stigberg <stig.bergestad@iterate.no>
2012-04-26 09:59:03 -0700

    First socket connection from client

--- src/main/java/no/iterate/graft/client/remote/GraftClient.java
+++ src/main/java/no/iterate/graft/client/remote/GraftClient.java
@@ -1,77 +1,44 @@
 package no.iterate.graft.client.remote;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import no.iterate.graft.Node;
+
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import jline.internal.InputStreamReader;
-
-import no.iterate.graft.Node;
-
 public class GraftClient {
 
-	private BufferedWriter writer;
-	private BufferedReader reader;
+	private Socket clientSocket;
 
-	public void connectTo(GraftServer server) throws UnknownHostException,
-			IOException {
-		String host = "localhost";
-		int portNumber = server.getPortNumber();
-		System.out.println("[client] Connected to server " + host + ":"
-				+ portNumber);
-
-		Socket socket = new Socket(host, portNumber);
-		writer = new BufferedWriter(new OutputStreamWriter(
-				socket.getOutputStream()));
-		reader = new BufferedReader(new InputStreamReader(
-				socket.getInputStream()));
+	public void connectTo(int port) throws IOException {
+		clientSocket = new Socket("localhost", port);
 	}
 
-	public Node createNode() throws IOException {
-		sendToServer(GraftServer.CREATE_NODE_COMMAND);
-
-		String id = reader.readLine();
-		return new Node(id, null);
+	public Node createNode() {
+		return null;
 	}
 
 	public void kill() {
-		sendToServer(GraftServer.SHUTDOWN_COMMAND);
 	}
 
-	public Node getNodeById(String id) throws IOException {
-		sendToServer(GraftServer.GET_NODE_BY_ID_COMMAND + " " + id);
-
-		String nodeString = reader.readLine();
-		System.out.println("[client] getNodeById> " + nodeString);
-		Node recievedNode = createFromString(nodeString);
-
-		return recievedNode;
+	public Node getNodeById(String id) {
+		return null;
 	}
 
-	private void sendToServer(String command) {
+	public void setReplica(int i) {
+	}
+
+	public String ping() {
 		try {
-			writer.write(command);
-			writer.newLine();
+			OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+			writer.write("PING");
+			writer.write("\n");
 			writer.flush();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			return reader.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
+			return "ERROR";
 		}
-		System.out.println("[client] SendToServer> " + command);
 	}
-
-	private Node createFromString(String id) {
-		Node receivedNode = new Node(id, null);
-		return receivedNode;
-	}
-
-	public void setReplica(int port) throws IOException {
-		sendToServer("setReplica " + port);
-		
-		String result = reader.readLine();
-		System.out.println("[client] addReplicaResponse> " + result);
-	}
-
 }
--- src/test/java/no/iterate/graft/client/remote/GraftReplicationRemoteTest.java
+++ src/test/java/no/iterate/graft/client/remote/GraftReplicationRemoteTest.java
@@ -3,20 +3,30 @@ package no.iterate.graft.client.remote;
 import static org.junit.Assert.*;
 import no.iterate.graft.Node;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class GraftReplicationRemoteTest {
 
 	@Test
 	public void replicateDataOverSocket() throws Exception {
-		GraftClient client = new GraftClient();
 		GraftServer first = GraftServer.start(1234);
 		GraftServer second = GraftServer.start(1235);
 		first.addReplica(second);
-		client.connectTo(first);
+
+		GraftClient client = new GraftClient();
+		client.connectTo(1234);
 		Node created = client.createNode();
 		client.kill();
-		client.connectTo(second);
+		client.connectTo(1235);
 		Node fetched = client.getNodeById(created.getId());
 		assertEquals(created.get("id"), fetched.get("id"));
 		
@@ -24,19 +34,58 @@ public class GraftReplicationRemoteTest {
 		client.kill();
 	}
 	
-	
+	@Ignore
 	@Test
 	public void setReplicaByCommand() throws Exception {
 		GraftClient client = new GraftClient();
 		GraftServer first = GraftServer.start(1234);
 		GraftServer second = GraftServer.start(1235);
-		client.connectTo(first);
+		client.connectTo(1234);
 		client.setReplica(1235);
 		Node created = client.createNode();
 		client.kill();
-		client.connectTo(second);
+		client.connectTo(1235);
 		Node fetched = client.getNodeById(created.getId());
 		assertEquals(created.get("id"), fetched.get("id"));
+	}
+
+	@Test
+	public void clientConnectsToSocket() throws IOException, InterruptedException {
+		Thread serverThread = new Thread() {
+			public void run() {
+				try {
+					ServerSocket server = new ServerSocket(3456);
+					server.setSoTimeout(100);
+					Socket socket = server.accept();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					String message = reader.readLine();
+					String response = message.equals("PING") ? "OK" : "ERROR";
+					OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream());
+					writer.write(response);
+					writer.write("\n");
+					writer.flush();
+					reader.close();
+					writer.close();
+					socket.close();
+					server.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		serverThread.start();
+		Thread.sleep(10);
+		GraftClient client = new GraftClient();
+		client.connectTo(3456);
+		String response = client.ping();
+		assertEquals("OK", response);
+	}
+
+	@Test(expected=SocketTimeoutException.class)
+	public void whatTheHellDoesSOTimeoutDo() throws IOException {
+		ServerSocket s = new ServerSocket(12334);
+		s.setSoTimeout(1);
+		s.accept();
 	}
 
 }
