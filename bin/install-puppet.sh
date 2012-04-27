8b64b749e1942d3ea3dbf6bf9f9bd223fe862b0e
Jakub Holy <jakub.holy@iterate.no>
2012-04-27 15:59:02 -0700

    Removed VPC, improved run-cluster to print hostname+id
    - instances in VPC haven't access to the net (unless they have an
    elastic IP or there is a NAT server in the VPC) => server start
    succeeded only when eIP assignment happened before cloud-init finished
    trying to access repositories
    - removed support for testclient cluster

--- bin/install-puppet.sh
+++ bin/install-puppet.sh
@@ -1,6 +1,2 @@
-wget http://rubyforge.org/frs/download.php/76032/rubygems-1.8.23.tgz
-tar xzf rubygems-1.8.23.tgz
-ruby rubygems-1.8.23/setup.rb
-gem install --no-ri --no-rdoc puppet -v 2.6.15
-mkdir -p /etc/puppet
+yum install puppet
 wget -O /etc/puppet/puppet.conf https://raw.github.com/iterate/codecamp2012/puppet/puppet.conf
--- bin/run-cluster.sh
+++ bin/run-cluster.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
-if [ $# -ne 2 ]
-then
-    echo "Usage: $0 <cluster name: database|testclient> <server count parameter>"
-    exit -1
-fi
-if [ "$1" = "database" ]; then
-	export CLUSTER=database
-	export GROUP=sg-15001f79 #codecamp-databases
-elif [ "$1" = "testclient" ]; then
-	export CLUSTER=testclient
-	export GROUP=sg-13001f7f #codecamp-testclients
-else
-	echo "Invalid cluster name '$1'; expected: database or testclient"
-	exit -1
-fi
-COUNT=$2
 
-if [ -z "EC2_PRIVATE_KEY" ]; then echo "ERROR: EC2_PRIVATE_KEY not set, make sure to include .bashrc to cinfigure EC2 access"; exit -1; fi
+COUNT=1
+if [ $# -eq 1 ]; then
+	COUNT=$1
+elif [ $# -gt 1 ]; then
+    echo "Usage: $0 [<server count parameter>]"
+    exit 1
+fi
 
-# See http://aws.amazon.com/ec2/instance-types/
+export CLUSTER=database
+export GROUP=sg-c3756fb7 # quick-start-1, same as puppet master
+
+if [ -z "EC2_PRIVATE_KEY" ]; then echo "ERROR: EC2_PRIVATE_KEY not set, make sure to include .bashrc to cinfigure EC2 access"; exit 1; fi
+if [ "`id -un`" != "root" ]; then echo 'ERROR: You must run this as root'; exit 1; fi
+
+## See http://aws.amazon.com/ec2/instance-types/
 #export INSTANCE_TYPE=t1.micro	# 613 MB, small CU with up to 2 CU in bursts
 export INSTANCE_TYPE=m1.small	# 1.7 GB, 1 CU
 #export INSTANCE_TYPE=m1.medium	# 3.75 GB, 2 CU
 
 export PUPPET_MASTER=ec2-176-34-218-57.eu-west-1.compute.amazonaws.com
-#export PUPPET_MASTER=ip-10-56-71-201.eu-west-1.compute.internal
 
 read -r -d '' USER_DATA <<EOF
 #cloud-config
+
+packages:
+ - puppet
 
 runcmd:
  - wget -O /etc/puppet/puppet.conf https://raw.github.com/iterate/codecamp2012/puppet/install-puppet.sh
@@ -35,17 +33,22 @@ runcmd:
  - FACTER_clustername=$CLUSTER puppetd -v --server $PUPPET_MASTER -w 60
 EOF
 
+echo "Going to run cluster of $COUNT instance(s)..."
+
 # See http://docs.amazonwebservices.com/AWSEC2/latest/CommandLineReference/ApiReference-cmd-RunInstances.html
 AMI=ami-3850624c
-OUT=$(ec2-run-instances $AMI -n $COUNT -g  $GROUP -k StarClusterCodeCamp12 -d "$USER_DATA" --instance-type $INSTANCE_TYPE --subnet subnet-bd1732d4)
+OUT=$(ec2-run-instances $AMI -n $COUNT -g $GROUP -k StarClusterCodeCamp12 -d "$USER_DATA" --instance-type $INSTANCE_TYPE)
 
 # AWK: RS is EOL regexp; RT captures the last RS found and is thus either INSTANCE or '' for the last record not followed by this
 # OBS: Doesn't work if there is no instance at all in the output
-INSTANCE=$(echo $OUT | awk 'BEGIN {RS="INSTANCE"} { if (RT != "INSTANCE") {print $1 }}')
+INSTANCE=$(echo "$OUT" | awk '/INSTANCE/ { print $1 }')
 
-echo "Waiting for the 1st instance to come up..."
-while ! ec2-describe-instances | awk "/$INSTANCE.*?pending/ {print \"Instance $INSTANCE not running yet...; LINE: \" \$0; exit 1}"; do sleep 4; done
-
-echo "Going to assign IP 46.51.196.99 to the instance '$INSTANCE': ec2-associate-address -a eipalloc-23e1c54a -i $INSTANCE"
-ec2-associate-address -a eipalloc-23e1c54a -i $INSTANCE
-
+echo "Waiting for the 1st instance to come up... (id=$INSTANCE)"
+while true; do
+       DESCR=$(ec2-describe-instances)
+       echo "$DESCR" | awk "/$INSTANCE.*?pending/ {exit 1}" && break
+       echo "Instance $INSTANCE. pending, waiting..."
+       sleep 4
+done
+echo "Cluster instance hostnames:"
+echo "$DESCR" | awk '/INSTANCE.*StarClusterCodeCamp12/ {print $4 " for id " $2}'
